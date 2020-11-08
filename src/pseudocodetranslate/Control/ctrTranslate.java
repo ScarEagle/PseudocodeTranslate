8fc451081567752ea9401c457e5d27998656d711
scarmona <sebastiansca10@gmail.com>
2020-11-08 13:37:18 -0500

    Se agregan regex para ignorar las líneas comentadas evitando poner punto y coma en esas líneas

--- src/pseudocodetranslate/Control/ctrTranslate.java
+++ src/pseudocodetranslate/Control/ctrTranslate.java
@@ -93,7 +93,12 @@ public class ctrTranslate {
     
     private void SetSemicolon(){
         String code;
+        List<String[]> pat;
         for (int i = 0; i < newCode.size(); i++) {
+            //replace lines with comments
+            newCode.set(i, newCode.get(i).replaceAll("\\/.+\\/", ""));
+            newCode.set(i, newCode.get(i).replaceAll("\\/*\\/.+", ""));
+            newCode.set(i, newCode.get(i).replaceAll(".+\\*\\/", ""));
             code = newCode.get(i).trim();
             if (!code.isEmpty()) {
                 if (!code.endsWith(":") && !code.endsWith("{") && !code.endsWith("}") && !code.endsWith(";")) {
